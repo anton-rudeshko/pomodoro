edf8268379f10b1264c4b493445f7d9673a49428
Anton Rudeshko <rudeshko@yandex-team.ru>
2013-11-09 19:16:21 +0400

    More stable countdown with setInterval

--- main.js
+++ main.js
@@ -6,14 +6,11 @@
     SECOND = 1000,
     MINUTE = 60 * SECOND,
 
-    POMODORO_DURATION = 25 * MINUTE,
+    POMODORO_DURATION = .25 * MINUTE,
     SHORT_BREAK_DURATION = 5 * MINUTE,
     LONG_BREAK_DURATION = 15 * MINUTE,
 
-    needStopTimer = false,
-    timeout = null,
-
-    countdownEl = byId('timer'),
+    countdownEl = byId('countdown'),
     pomodorosTodayEl = byId('pomodoros-today'),
 
     pomodoroButton = byId('start-pomodoro'),
@@ -32,78 +29,104 @@
 
   function formatTime(time) {
     var mins = Math.floor(time / MINUTE),
-      secs = Math.floor(time % MINUTE / SECOND);
+      secs = Math.round(time % MINUTE / SECOND);
     return pad(mins) + ':' + pad(secs);
   }
 
-  function startTimer(time) {
-    if (timeout) return;
+  function Countdown(options) {
+    options || (options = {});
 
-    var startMillis = new Date();
-    needStopTimer = false;
+    this.tick = options.tick || SECOND;
+    this.onTick = options.onTick;
+    this.onEnd = options.onEnd;
 
-    (function updateTimer() {
-      var timeDiff = new Date() - startMillis,
-        timeLeft = time - timeDiff;
-
-      if (timeLeft <= 0) {
-        timeLeft = 0;
-        needStopTimer = true;
-      }
-
-      countdownEl.innerText = formatTime(timeLeft);
-
-      if (timeLeft === 0) {
-        pomodorosTodayEl.innerText = 'Pomodoros today: ' + ++pomodorosToday;
-        notify();
-      }
-
-      if (needStopTimer)
-        stopTimer();
-      else
-        timeout = setTimeout(updateTimer, 250)
-    })();
+    this._countdownInterval = null;
   }
 
-  function stopTimer() {
-    clearTimeout(timeout);
-    timeout = null;
-  }
+  Countdown.prototype = {
+    startCountdown: function(countdownTime) {
+      if (this._countdownInterval) return;
 
-  function notify() {
-    return new Notification('Pomodoro tracker', {
-      tag: 'pomodoro-tracker',
-      body: 'Time is up!'
-    });
-  }
+      this._countdownTime = countdownTime;
+      this._startMillis = new Date();
+
+      var _this = this;
+
+      this._updateCountdown();
+      this._countdownInterval = setInterval(function() {
+        _this._updateCountdown();
+      }, this.tick);
+    },
+
+    stopCountdown: function() {
+      clearInterval(this._countdownInterval);
+      this._countdownInterval = null;
+    },
+
+    restartCountdown: function(countdownTime) {
+      this.stopCountdown();
+      this.startCountdown(countdownTime);
+    },
+
+    _updateCountdown: function() {
+      var timeDiff = new Date() - this._startMillis,
+        timeLeft = this._countdownTime - timeDiff;
+
+      this._reportTick(timeLeft < 0 ? 0 : timeLeft);
+    },
+
+    _reportTick: function(timeLeft) {
+      typeof this.onTick === 'function' && this.onTick(timeLeft);
+      if (timeLeft === 0) this._reportEnd();
+    },
+
+    _reportEnd: function() {
+      this.stopCountdown();
+      typeof this.onEnd === 'function' && this.onEnd();
+    }
+
+  };
+
+  var countdown = new Countdown({
+    tick: SECOND,
+    onTick: function(timeLeft) {
+      var formattedTime = formatTime(timeLeft);
+      document.title = formattedTime + ' - Pomodoro tracker';
+      countdownEl.innerText = formattedTime;
+    },
+    onEnd: function() {
+      pomodorosTodayEl.innerText = 'Pomodoros today: ' + ++pomodorosToday;
+
+      document.title = 'Time is up! - Pomodoro tracker';
+      new Notification('Pomodoro tracker', {
+        tag: 'pomodoro-tracker',
+        body: 'Time is up!'
+      });
+    }
+  });
 
   taskNameForm.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    startTimer(POMODORO_DURATION);
+    countdown.startCountdown(POMODORO_DURATION);
     taskNameInput.blur();
   });
 
   pomodoroButton.addEventListener('click', function(e) {
-    stopTimer();
-    startTimer(POMODORO_DURATION);
+    countdown.restartCountdown(POMODORO_DURATION);
   });
 
   shortBreakButton.addEventListener('click', function(e) {
-    stopTimer();
-    startTimer(SHORT_BREAK_DURATION);
+    countdown.restartCountdown(SHORT_BREAK_DURATION);
   });
 
   longBreakButton.addEventListener('click', function(e) {
-    stopTimer();
-    startTimer(LONG_BREAK_DURATION);
+    countdown.restartCountdown(LONG_BREAK_DURATION);
   });
 
   notifyMeButton.addEventListener('click', function(e) {
     Notification.requestPermission(function(permission) {
     })
   })
-
-
 
 })(document);
